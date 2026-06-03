6e3b9357c426d7102bbb92fd48e2d64557d8ea3d
Vamsi_Kalla <vnalnvamsi@gmail.com>
2026-06-03 22:56:48 +0530

    strings

---
+++ Javascirpt_Basics_Practice/EX_14.String Methods/152_Count vowels in a string.js
@@ -0,0 +1,19 @@
+// Count vowels
+let str = "JavaScript";
+let count = 0;
+
+for (let i = 0; i < str.length; i++) {
+    let ch = str.charAt(i).toLowerCase();
+
+    if (
+        ch === "a" ||
+        ch === "e" ||
+        ch === "i" ||
+        ch === "o" ||
+        ch === "u"
+    ) {
+        count++;
+    }
+}
+
+console.log(count);
