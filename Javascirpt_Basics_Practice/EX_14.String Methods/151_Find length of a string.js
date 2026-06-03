c32b6554f4f87115c5589c1f8833bba94241a003
Vamsi_Kalla <vnalnvamsi@gmail.com>
2026-06-03 22:23:04 +0530

    strings

---
+++ Javascirpt_Basics_Practice/EX_14.String Methods/151_Find length of a string.js
@@ -0,0 +1,20 @@
+let str = "JavaScript";
+console.log(str.length);
+
+// Convert string to uppercase
+let str = "hello";
+console.log(str.toUpperCase());
+
+// Check whether string contains "QA"
+let str = "QA Automation Engineer";
+console.log(str.includes("QA"));
+
+// 4. Reverse a string
+let str = "smile";
+let rev = "";
+
+for (let i = str.length - 1; i >= 0; i--) {
+    rev = rev + str.charAt(i);
+}
+
+console.log(rev); 
