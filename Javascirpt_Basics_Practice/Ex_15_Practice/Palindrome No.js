5ac766eb90cfbe42cd90894a59f9a10b5841dcda
Vamsi_Kalla <vnalnvamsi@gmail.com>
2026-06-04 22:56:27 +0530

    Practice programs

---
+++ Javascirpt_Basics_Practice/Ex_15_Practice/Palindrome No.js
@@ -0,0 +1,49 @@
+// A Palindrome Number is a number that reads the same forward and backward.
+
+// Method 1: Using a Loop
+let num = 150;
+let originalNum = num;
+let reversed = 0;
+
+while (num > 0) {
+    let digit = num % 10;
+    reversed = reversed * 10 + digit;
+    num = Math.floor(num / 10);
+}
+
+if (originalNum === reversed) {
+    console.log("Palindrome");
+} else {
+    console.log("Not Palindrome");
+}
+
+// --------------------------------------------
+// Method 2: Using String (Shorter)
+
+let num = 121;
+
+let reversed = num.toString().split('').reverse().join('');
+
+if (num.toString() === reversed) {
+    console.log("Palindrome");
+} else {
+    console.log("Not Palindrome");
+}
+
+// Method - 3 using function
+
+function isPalindrome(num) {
+    let originalNum = num;
+    let reversed = 0;
+
+    while (num > 0) {
+        let digit = num % 10;
+        reversed = reversed * 10 + digit;
+        num = Math.floor(num / 10);
+    }
+
+    return originalNum === reversed;
+}
+
+console.log(isPalindrome(121)); // true
+console.log(isPalindrome(123)); // false
---
+++ Javascirpt_Basics_Practice/Ex_15_Practice/Palindrome String.js
@@ -0,0 +1,44 @@
+// Method 1: Using a Loop
+
+let str = "madam";
+
+let reversed = "";
+
+for (let i = str.length - 1; i >= 0; i--) {
+    reversed = reversed + str.charAt(i);
+}
+
+if (str === reversed) {
+    console.log("Palindrome");
+} else {
+    console.log("Not Palindrome");
+}
+
+// Method 2: Using Two Pointers (Interview Favorite)
+
+let str = "madam";
+let isPalindrome = true;
+
+for (let i = 0; i < str.length / 2; i++) {
+    if (str.charAt(i) !== str.charAt(str.length - 1 - i)) {
+        isPalindrome = false;
+        break;
+    }
+}
+
+console.log(isPalindrome ? "Palindrome" : "Not Palindrome");
+
+
+// Function version
+function isPalindrome(str) {
+    let reversed = "";
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed = reversed + str.charAt(i);
+    }
+
+    return str === reversed;
+}
+
+console.log(isPalindrome("madam")); // true
+console.log(isPalindrome("hello")); // false
