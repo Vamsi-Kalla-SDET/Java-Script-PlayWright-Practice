8746649358497d71236ea898ec3ff154cd0ce56d
Vamsi_Kalla <vnalnvamsi@gmail.com>
2026-06-09 21:17:30 +0530

    arrows

---
+++ 2.ES6 Features/3.Arrow Functions.js
@@ -0,0 +1,18 @@
+// Normal Function
+
+function add(a, b) {
+    return a + b;
+}
+console.log (add(5,6))
+
+// Arrow Function
+
+const add = (a, b) => {
+    return a + b;
+};
+console.log (add(5,8))
+
+// Short Form
+
+const add = (a, b) => a + b;
+console.log (add(6,6))
