0832591c0107ccc4da266152411b5f467bf84eb2
Vamsi_Kalla <vnalnvamsi@gmail.com>
2026-06-09 22:55:58 +0530

    arrows

--- 2.ES6 Features/3.Arrow Functions.js
+++ 2.ES6 Features/3.Arrow Functions.js
@@ -16,3 +16,38 @@ console.log (add(5,8))
 
 const add = (a, b) => a + b;
 console.log (add(6,6))
+
+
+// Sum of two numbers
+let a = 5, b = 6;
+let sum = a + b;
+console.log(sum);
+
+const mul = (a, b) => a * b;
+console.log (mul(5,6));
+console.log (mul(3,6));
+
+// Largest of two numbers
+
+const largest = (a, b) => {
+    if (a > b) {
+        return a;
+    } else if (b > a) {
+        return b;
+    } else {
+        return "Both are equal";
+    }
+};
+
+console.log(largest(10, 20));
+console.log(largest(10, 50));
+
+// Square of a number
+
+let  a = 4;
+let sq = a * a;
+console.log(sq);
+
+const sq = (a) => (a * a);
+console.log(sq(5));
+
