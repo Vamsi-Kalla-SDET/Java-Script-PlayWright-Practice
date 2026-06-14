a700234285db7103a7ddf04f0d29c8375c9312e6
Vamsi_Kalla <vnalnvamsi@gmail.com>
2026-06-14 22:29:18 +0530

    Updated

--- 2.ES6 Features/5.Spread Operator.js
+++ 2.ES6 Features/5.Spread Operator.js
@@ -1,3 +1,5 @@
+// Copy an Array
+
 let arr1 = [1, 2, 3];
 let arr2 = [...arr1];
 
@@ -11,3 +13,48 @@ let b = [3,4];
 let c = [...a, ...b];
 
 console.log(c);
+
+
+// The Spread Operator (...) expands an array, object, or string into individual elements.
+
+// 3. Add Elements to an Array
+
+const numbers = [2, 3, 4];
+
+const result = [1, ...numbers, 5];
+
+console.log(result);
+
+// 4. Find Largest Number in Array
+const nums = [10, 25, 8, 40, 15];
+
+console.log(Math.max(...nums));
+
+
+// Copy an Object
+
+const person = {
+    name: "John",
+    age: 25
+};
+
+const copy = { ...person };
+
+console.log(copy);
+
+
+// Update Object Values
+
+const user = {
+    name: "John",
+    age: 25
+};
+
+const updatedUser = {
+    ...user,
+    age: 30
+};
+
+console.log(updatedUser);
+
+
