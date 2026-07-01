9ec061c987df3678c1c4795074b118bea0066d70
Vamsi_Kalla <vnalnvamsi@gmail.com>
2026-07-01 21:00:25 +0530

    updated

---
+++ 4.Promises/6.Example 6 - Student Pass-Fail.js
@@ -0,0 +1,17 @@
+let marks = 70;
+
+let result = new Promise(function (resolve, reject) {
+  if (marks >= 35) {
+    resolve("Student Passed");
+  } else {
+    reject("Student Failed");
+  }
+});
+
+result
+  .then(function (msg) {
+    console.log(msg);
+  })
+  .catch(function (msg) {
+    console.log(msg);
+  });
